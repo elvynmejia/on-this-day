7ad377952de166919bcec2b303570ed81d79b5ee
Elvyn Mejia <elvynmejia@gmail.com>
2022-12-07 09:29:42 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
-function App() {
+import Container from '@mui/material/Container';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  const theme = createTheme({
+    palette: {
+      mode: 'light',
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <p>On This Day</p>
+        </Container>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
