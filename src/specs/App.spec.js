63f88189b43b2b0d57c0969b288b26d110915d48
Elvyn Mejia <elvynmejia@gmail.com>
2022-12-07 23:27:27 -0800

    adds a describe block to App tests

--- src/specs/App.spec.js
+++ src/specs/App.spec.js
@@ -21,70 +21,72 @@ afterEach(async () => {
 
 afterAll(() => server.close());
 
-test('renders application with default state', async () => {
-  render(<App />);
-  const button = screen.getByText(/Discover today's birthdays/i);
-  expect(button).toBeInTheDocument();
+describe('App', () => {
+  test('renders application with default state', async () => {
+    render(<App />);
+    const button = screen.getByText(/Discover today's birthdays/i);
+    expect(button).toBeInTheDocument();
 
-  await waitFor(() => {
-    const links = screen.queryAllByRole('link');
-    expect(links.length).toBe(0);
+    await waitFor(() => {
+      const links = screen.queryAllByRole('link');
+      expect(links.length).toBe(0);
+    });
   });
-});
 
-test('renders a list when button in clicked', async () => {
-  render(<App />);
+  test('renders a list when button in clicked', async () => {
+    render(<App />);
 
-  const button = screen.getByText(/Discover today's birthdays/i);
+    const button = screen.getByText(/Discover today's birthdays/i);
 
-  expect(button).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
 
-  await userEvent.click(button);
-
-  expect(
-    screen.getByRole('progressbar')
-  ).toBeInTheDocument();
-
-  await waitFor(() => {
-    const links = screen.getAllByRole('link');
+    await userEvent.click(button);
 
     expect(
-      screen.getByText(/2020 - Dottie and Gee Gee/i)
+      screen.getByRole('progressbar')
     ).toBeInTheDocument();
 
-    expect(
-      screen.getByRole('link')
-    ).toHaveAttribute('href', 'url');
+    await waitFor(() => {
+      const links = screen.getAllByRole('link');
 
-    expect(links.length).toBe(1);
+      expect(
+        screen.getByText(/2020 - Dottie and Gee Gee/i)
+      ).toBeInTheDocument();
+
+      expect(
+        screen.getByRole('link')
+      ).toHaveAttribute('href', 'url');
+
+      expect(links.length).toBe(1);
+    });
   });
-});
 
-test('shows error modal when data fetch fails', async () => {
-  render(<App queryClient={testQueryClient} />);
+  test('shows error modal when data fetch fails', async () => {
+    render(<App queryClient={testQueryClient} />);
 
-  server.use(
-    rest.get(url, (req, res, ctx) => {
-      return res.once(
-        ctx.status(400),
-        ctx.json({}),
-      )
-    })
-  );
+    server.use(
+      rest.get(url, (req, res, ctx) => {
+        return res.once(
+          ctx.status(400),
+          ctx.json({}),
+        )
+      })
+    );
 
-  const button = screen.getByText(/Discover today's birthdays/i);
+    const button = screen.getByText(/Discover today's birthdays/i);
 
-  expect(button).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
 
-  await userEvent.click(button);
+    await userEvent.click(button);
 
-  expect(
-    screen.getByRole('progressbar')
-  ).toBeInTheDocument();
-
-  await waitFor(() => {
     expect(
-      screen.getByText(/Error retreiving today's birthdays. Try again/i)
+      screen.getByRole('progressbar')
     ).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(/Error retreiving today's birthdays. Try again/i)
+      ).toBeInTheDocument();
+    });
   });
 });
